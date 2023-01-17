9b4d2c3e18db19f48f2e637d19404572c9173991
trinhvanvinh <trinhvanvinh018>
2023-01-17 07:36:05 +0700

    compl guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,31 @@
+use colored::*;
+use rand::Rng;
+use std::{cmp::Ordering, io};
+
 fn main() {
-    println!("Hello, world!");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("secret number: {}", secret_number);
+    loop {
+        println!("Please input keyboard");
+
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to readline");
+
+        println!("Your guess: {}", guess);
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("{}", "To Small".red()),
+            Ordering::Greater => println!("{}", "To Big".red()),
+            Ordering::Equal => {
+                println!("{}", "You win!!!".green());
+                break;
+            }
+        }
+    }
 }
