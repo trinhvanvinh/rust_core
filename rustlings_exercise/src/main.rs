2f9cd79392c5802e1a49ddb699bbd5c4cb5cfc3a
trinhvanvinh <trinhvanvinh018@gmail.com>
2023-01-26 10:23:30 +0700

    add ruslings

---
+++ rustlings_exercise/src/main.rs
@@ -0,0 +1,23 @@
+use std::f32;
+
+fn main() {
+    // let pi = 3.14f32;
+    // let radius = 5.00f32;
+
+    // let area = pi * f32::powi(radius, 2);
+
+    // println!(
+    //     "The area of a circle with radius {:.2} is {:.5}!",
+    //     radius, area
+    // )
+
+    let pi = f32::consts::PI;
+    let radius = 5.00f32;
+
+    let area = pi * f32::powi(radius, 2);
+
+    println!(
+        "The area of a circle with radius {:.2} is {:.5}!",
+        radius, area
+    )
+}
