6c16996e09eb0a4da0dff534b1d773245b6e6ca6
trinhvanvinh <trinhvanvinh018@gmail.com>
2023-01-27 08:46:50 +0700

    a

---
+++ rustlings_exercise/target/debug/deps/rustlings_exercise-1ff948f168252b60.d
@@ -0,0 +1,5 @@
+/Users/vinhtrinhvan/Desktop/rust-core/rust_core/rustlings_exercise/target/debug/deps/rustlings_exercise-1ff948f168252b60: src/main.rs
+
+/Users/vinhtrinhvan/Desktop/rust-core/rust_core/rustlings_exercise/target/debug/deps/rustlings_exercise-1ff948f168252b60.d: src/main.rs
+
+src/main.rs:
---
+++ rustlings_exercise/target/debug/deps/rustlings_exercise-500acc477ba26a24.d
@@ -0,0 +1,5 @@
+/Users/vinhtrinhvan/Desktop/rust-core/rust_core/rustlings_exercise/target/debug/deps/rustlings_exercise-500acc477ba26a24.rmeta: src/main.rs
+
+/Users/vinhtrinhvan/Desktop/rust-core/rust_core/rustlings_exercise/target/debug/deps/rustlings_exercise-500acc477ba26a24.d: src/main.rs
+
+src/main.rs:
