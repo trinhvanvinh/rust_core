c4734f495e7f158f93df76634e537df63ec91773
trinhvanvinh <trinhvanvinh018>
2023-01-29 19:37:02 +0700

    error3

--- rustlings_exercise/src/main.rs
+++ rustlings_exercise/src/main.rs
@@ -373,7 +373,32 @@ impl State {
 }*/
 
 // === error2 ===
+/*use std::num::ParseIntError;
+pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
+    let processing_fee = 1;
+    let cost_per_item = 5;
+    let qty = item_quantity.parse::<i32>()?;
+
+    Ok(qty * cost_per_item + processing_fee)
+}*/
+
+// === error3 ===
 use std::num::ParseIntError;
+
+fn main() -> Result<(), ParseIntError> {
+    let mut tokens = 100;
+    let pretend_user_input = "8";
+    let cost = total_cost(pretend_user_input)?;
+
+    if cost > tokens {
+        println!("You can't afford that many");
+    } else {
+        tokens -= cost;
+        println!("You now have {} tokens ", tokens);
+    }
+    Ok(())
+}
+
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let processing_fee = 1;
     let cost_per_item = 5;
